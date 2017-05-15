a1a918a6f9b455e31588c547583d811b3dc4fd6c
ChoiSiwon <csw1511@naver.com>
2017-05-15 09:07:25 +0000

    Message

--- app/controllers/drunktiger_controller.rb
+++ app/controllers/drunktiger_controller.rb
@@ -1,6 +1,5 @@
 class DrunktigerController < ApplicationController
     def index
-        @foods  = ['피자','치킨','짬뽕','탕수육','햄버거','술','족발']
-        @real = ['뭘먹게될까?','이거말고 딴거먹어라']
+        @foods  = ['밥먹기','잠자기','공부하기','게임하기','전공공부하기','방안에만 박혀있지말고 나가서 사람들좀 만나기','제대로 된 식사를 하기']
     end
 end
